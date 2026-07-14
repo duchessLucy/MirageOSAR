bab2d84c35976c7357f3a9a73a33227c7ccc8e1b
duchessLucy <duchess.lucy@hotmail.com>
2026-07-13 20:20:58 -0600

    Create battleSpaceShipSolarSystem.cxx with map setup
    Initialize battleFieldSolarSystem map with various entries.

---
+++ battleSpaceShipSolarSystem.cxx
@@ -0,0 +1,21 @@
+#include<map>
+#include<iostream>
+#include<string>
+#include<string_view>
+int main(int argc, char *argv[])
+{
+        std::map<std::string,int>map, battleFieldSolarSystem;
+
+        battleFieldSolarSystem["instance"]=1;
+        battleFieldSolarSystem["star"]=2;
+        battleFieldSolarSystem["planets"]=8;
+        battleFieldSolarSystem["tickle warfare technology"]=3;
+        battleFieldSolarSystem["moon"]=short();
+        battleFieldSolarSystem["anomalies"]=5;
+        battleFieldSolarSystem["mining destination"]=8;
+        battleFieldSolarSystem["station"]=4;
+        battleFieldSolarSystem["tw battle spaceship"]=int();
+
+
+        return 0;
+}
